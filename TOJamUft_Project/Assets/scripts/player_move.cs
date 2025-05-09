430bbb645835193975d6f53feccdfc9c9a21b197
finesden33 <e6fine@gmail.com>
2025-05-09 14:36:40 -0400

    player car basics

---
+++ TOJamUft_Project/Assets/scripts/player_move.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class player_move : MonoBehaviour
+{
+    private float _playerInput;
+    private float _rotationInput;
+    private Vector3 _userRot;
+    private bool _userJumped;
+
+    private const float _inputScale = 0.5f;
+
+    private Transform _transform;
+    private Rigidbody _rigidbody;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        _transform = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _playerInput = Input.GetAxis("Vertical");
+        _rotationInput = Input.GetAxis("Horizontal");
+        // _userJumped = Input.GetButton("Jump");
+    }
+
+    private void FixedUpdate()
+    {
+        _userRot = _transform.rotation.eulerAngles;
+        _userRot += new Vector3(0, _rotationInput, 0);
+        _transform.rotation = Quaternion.Euler(_userRot);
+        _rigidbody.linearVelocity += _transform.forward * _playerInput * _inputScale;
+
+        // if (_userJumped)
+        // {
+        //     _rigidbody.AddForce(Vector3.up, ForceMode.VelocityChange);
+        //     _userJumped = false;
+        // }
+    }
+}
+
