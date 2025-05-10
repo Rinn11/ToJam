fdd3f727e3e7a0cc25adfa0c50bb6cf4dc6ba505
M1ttttw <matt_tay2003@outlook.com>
2025-05-09 21:51:11 -0400

    Fixed a bug for level gen

--- TOJamUft_Project/Assets/Scripts/TileSpawner.cs
+++ TOJamUft_Project/Assets/Scripts/TileSpawner.cs
@@ -12,17 +12,19 @@ public class TileSpawner : MonoBehaviour
     private int tilePassedCounter = 0;
 
     public void crossed(SectionTrigger sec) {
-        // Once we cross a section trigger, we increment the coutner
+        // Once we cross a section trigger, we increment the counter
         tilePassedCounter++;
 
         // Use the counter to check if we need to spawn the tileSpawnAmount of tiles
         if (tilePassedCounter >= tilePassedCount) {
             tilePassedCounter = 0;
-            GameObject newSection = Instantiate(tileSection, sec.tileSpawnPoint.transform.position, sec.transform.rotation, gameObject.transform);
+            // Grab the most recent tile section (usually the last child of the tile manager)
+            GameObject lastTileSection = gameObject.transform.GetChild(gameObject.transform.childCount - 1).gameObject;
+            GameObject newSection = Instantiate(tileSection, lastTileSection.GetComponentInChildren<SectionTrigger>().tileSpawnPoint.transform.position, lastTileSection.transform.rotation, gameObject.transform);
             newSection.GetComponentInChildren<SectionTrigger>().tileManager = this;
             for (int i = 0; i < tileSpawnAmount - 1; i++) {
                 // Utilize the object's forward vector to make tile spawning invariant to the rotation it's facing.
-                newSection = Instantiate(tileSection, newSection.GetComponentInChildren<SectionTrigger>().tileSpawnPoint.transform.position, sec.transform.rotation, gameObject.transform);
+                newSection = Instantiate(tileSection, newSection.GetComponentInChildren<SectionTrigger>().tileSpawnPoint.transform.position, newSection.transform.rotation, gameObject.transform);
                 newSection.GetComponentInChildren<SectionTrigger>().tileManager = this;
             }
         } 
