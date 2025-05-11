9736c2325611a11e619ace4a00dc4ba5eaa4ee24
M1ttttw <matt_tay2003@outlook.com>
2025-05-10 22:02:22 -0400

    Added some SFX clips

---
+++ TOJamUft_Project/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public GameObject audioPlayer;
+    public float masterVolume;
+
+    public void playClip(AudioClip clip, float volume, GameObject requestedObject, bool loop = false) { 
+        // Create a new audio player object, which will contain a new AudioSource.
+        GameObject apCopy = Instantiate(audioPlayer, requestedObject.transform.position, requestedObject.transform.rotation, gameObject.transform);
+        AudioSource apSource = apCopy.GetComponent<AudioSource>();
+
+        // Place the clip in the audio player's source, and play it.
+        masterVolume = Mathf.Clamp(masterVolume, 0.0f, 1.0f);
+        apSource.clip = clip;
+        apSource.volume = volume * masterVolume;
+        apSource.loop = loop;
+        apSource.Play();
+    }
+
+    public GameObject createAudioPlayer(GameObject requestedObject) {
+        // If needed, create a new audio player object and return it to the requested object for custom use or behavior.
+        return Instantiate(audioPlayer, requestedObject.transform.position, requestedObject.transform.rotation, gameObject.transform);
+    }
+}
