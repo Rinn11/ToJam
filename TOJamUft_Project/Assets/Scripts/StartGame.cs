37d3ebda7b35083dfbab53770d141a5c2fe46c33
Rinn11 <davbrett@sheridancollege.ca>
2025-05-20 15:21:05 -0400

    Car spawner is now working as a prefab in level gen

--- TOJamUft_Project/Assets/Scripts/StartGame.cs
+++ TOJamUft_Project/Assets/Scripts/StartGame.cs
@@ -16,7 +16,7 @@ public class StartGame : MonoBehaviour
     {
         Time.timeScale = 0;
         
-        titleScreen = GameObject.Find("TitleScreenUI");
+        //titleScreen = GameObject.Find("TitleScreenUI");
         titleScreen.SetActive(true);
         
         audioSources = GetComponents<AudioSource>();
