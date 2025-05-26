99c93e6af9196e1f1c37bc18648d0c605f18f5a9
M1ttttw <matt_tay2003@outlook.com>
2025-05-26 00:03:54 -0400

    Round Manager Started
    We will try to utilize the event system in unity to add listeners, and decrease coupling.

---
+++ TOJamUft_Project/Assets/LifeCycle/Scripts/RoundManager.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+
+// RoundManager will be utilizing the event system to manage game rounds. It will have methods to start, end, and manage states of the round.
+// We define 1 game as 2 rounds, each round will end with a player swap once the player that is driving the cop car catches the other player.
+public class RoundManager : MonoBehaviour
+{
+    public int totalRounds = 2;
+    private int currentRound = 0;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void StartRound()
+    {
+
+    }
+
+    public void SwapPlayers()
+    {
+
+    }
+
+    public void EndRound()
+    {
+
+    }
+
+
+
+}
