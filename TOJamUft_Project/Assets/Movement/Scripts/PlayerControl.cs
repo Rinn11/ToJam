dc7defe0ca7e4da54f6c3093196bfb5559567dd0
JeremyXie123 <39686698+JeremyXie123@users.noreply.github.com>
2025-06-10 14:24:55 -0400

    Modify Movement To Use New Input System

--- TOJamUft_Project/Assets/Movement/Scripts/PlayerControl.cs
+++ TOJamUft_Project/Assets/Movement/Scripts/PlayerControl.cs
@@ -1,45 +1,19 @@
-/*
- * Passes input from the controller to the PlayerMove script
- */
-
-// TODO: Originally was used because Cop AI used the same movement model (PlayerMove) that the player used.
-// Since the cop is now another player, this may not be needed anymore.
-
-using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerControl : MonoBehaviour
 {
-    public PlayerSwapEventSender swapSender;
+    public MonoBehaviour movementScript; // Drag any script that implements IMovementModel
+    private IMovementModel movementModel;
 
-    public MonoBehaviour movementScript;    // So we can drag anything in
-    private IMovementModel movementModel;   // But we still need a class to run methods
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    [SerializeField] string _horizontalAxis;
-    [SerializeField] string _verticalAxis;
-
-    [SerializeField] string _horizontalAxisOpposite;
-    [SerializeField] string _verticalAxisOpposite;
+    [SerializeField] private PlayerInput playerInput;
 
     private bool locked = false;
 
-    void Start()
+    private void Start()
     {
         movementModel = movementScript as IMovementModel;
-    }
-
-    private void OnEnable()
-    {
-        if (swapSender != null)
-            swapSender.OnBoolEvent += recievePlayerSwap;
-    }
-
-    private void OnDisable()
-    {
-        if (swapSender != null)
-            swapSender.OnBoolEvent -= recievePlayerSwap;
+        playerInput = GetComponent<PlayerInput>();
     }
 
     public void SetLocked(bool newLocked)
@@ -47,32 +21,20 @@ public class PlayerControl : MonoBehaviour
         locked = newLocked;
     }
 
-
-    public void recievePlayerSwap(bool isPlayer1Driving)
+    private void Update()
     {
-        // Swap the horizontal and vertical axes
-        string tempHorizontal = _horizontalAxis;
-        string tempVertical = _verticalAxis;
+        if (playerInput == null || movementModel == null) return;
 
-        _horizontalAxis = _horizontalAxisOpposite;
-        _verticalAxis = _verticalAxisOpposite;
+        // Always pull from current action map
+        InputAction steerAction = playerInput.actions["Steer"];
+        if (steerAction == null) return;
 
-        _horizontalAxisOpposite = tempHorizontal;
-        _verticalAxisOpposite = tempVertical;
-        Debug.Log($"Swapped controls: {_horizontalAxis}, {_verticalAxis} <-> {_horizontalAxisOpposite}, {_verticalAxisOpposite}");
-    }
-
-    
-
-    // Update is called once per frame
-    void Update()
-    {
-        Vector2 moveValue = new Vector2(Input.GetAxis(_horizontalAxis), Input.GetAxis(_verticalAxis));
+        Vector2 steer = steerAction.ReadValue<Vector2>();
         if (locked)
         {
-            moveValue = Vector2.zero;
+            steer = Vector2.zero;
         }
-        movementModel.ProcessInputs(moveValue.x, moveValue.y);
+
+        movementModel.ProcessInputs(steer.x, steer.y);
     }
-    
 }
