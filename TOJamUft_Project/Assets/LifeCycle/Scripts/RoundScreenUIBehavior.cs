4f21ced6a64b0ac7e26d3db87e4f5a8ac0e28bbb
M1ttttw <matt_tay2003@outlook.com>
2025-06-23 23:05:31 -0400

    A start to the timer
    - I might refactor some of the round system code to be less coupled
    - Some of the current round system code is getting in the way of something quite simple.

--- TOJamUft_Project/Assets/LifeCycle/Scripts/RoundScreenUIBehavior.cs
+++ TOJamUft_Project/Assets/LifeCycle/Scripts/RoundScreenUIBehavior.cs
@@ -9,6 +9,7 @@ using UnityEngine.UI;
 public class RoundScreenUIBehavior : MonoBehaviour
 {
     public Text scoreText;
+    public Text timerText;
 
     // Count and update the score.
     public void updateScoreText(float score)
@@ -17,13 +18,20 @@ public class RoundScreenUIBehavior : MonoBehaviour
         scoreText.text = $"Fine: ${score}";
     }
 
+    // Update the timer text
+    public void updateTimerText(float timeLeft)
+    { 
+        Debug.Log($"Updating timer text with time left: {timeLeft}");
+        timerText.text = $"Time Left: {timeLeft:F2} seconds";
+    }
+
     // Show the whole round screen UI
     public void showRoundScreenUI()
     {
         // We need to activate the canvas object under the round screen UI, the object needs to be active for the event system to work.
         Transform child = transform.Find("Canvas");
         child.gameObject.SetActive(true);
-        
+
     }
 
     // Hide the whole round screen UI
