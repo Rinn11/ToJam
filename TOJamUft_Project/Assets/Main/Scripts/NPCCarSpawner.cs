155ad99de1ed75626c01940f3121bc3e6d9e18cc
M1ttttw <matt_tay2003@outlook.com>
2025-07-09 03:18:46 -0400

    Fixed a bug where the destination may change mid way if the car enters another travel point on accident

---
+++ TOJamUft_Project/Assets/Main/Scripts/NPCCarSpawner.cs
@@ -0,0 +1,45 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+// This script is to be attached to the Directed Graph representing the car pathing system.
+// It will handle the spawning of NPC cars at designated nodes in the graph.
+public class NPCCarSpawner : MonoBehaviour
+{
+    [Header("Car Prefabs List")]
+    public List<GameObject> carPrefabs; // List of car prefabs to spawn
+
+    [Header("Car Spawn Settings")]
+    public int numberOfCarsToSpawn; // Number of cars to spawn
+    public float initialCarSpeed;
+
+    [Header("Optional Settings")]
+    public Transform cloneParent; // Parent object for cloned cars (optional, can be used for organization)
+
+    private void Start()
+    {
+        // Grab the children of the graph (this object) and randomly spawn a car at one of those nodes.
+        for (int i = 0; i < numberOfCarsToSpawn; i++)
+        {
+            // Randomly select a car prefab from the list
+            GameObject carPrefab = carPrefabs[Random.Range(0, carPrefabs.Count)];
+
+            // Randomly select a spawn point from the children of this object
+            Transform spawnPoint = transform.GetChild(Random.Range(0, transform.childCount));
+
+            // Set the destination for the car to the next travel point
+            TravelPoint travelPoint = spawnPoint.GetComponent<TravelPoint>();
+            if (travelPoint != null && !travelPoint.triggered)
+            {
+                // Instantiate the car at the spawn point
+                GameObject carInstance = Instantiate(carPrefab, spawnPoint.position, spawnPoint.rotation, cloneParent);
+                CarAgent carAgent = carInstance.GetComponent<CarAgent>();
+                if (carAgent != null)
+                {
+                    // Set the speed
+                    carAgent.speed = initialCarSpeed;
+                    carAgent.destination = travelPoint; // Set the car's destination to the next travel point
+                }
+            }
+        }
+    }
+}
--- TOJamUft_Project/Assets/Main/Scripts/TravelPoint.cs
+++ TOJamUft_Project/Assets/Main/Scripts/TravelPoint.cs
@@ -1,9 +1,20 @@
 using System.Collections.Generic;
+using System.Data.Common;
 using UnityEngine;
 
 public class TravelPoint : MonoBehaviour
 {
     public List<TravelPoint> nextPoints;
+    [HideInInspector]
+    public bool triggered = false; // Flag to check if there is something already in the trigger
+
+    private string pointName; // Name or ID of the travel point
+
+    private void Start()
+    {
+        // Initialize the point name with the name of the GameObject
+        pointName = gameObject.name;
+    }
 
     public TravelPoint getSuccessor()
     {
@@ -16,16 +27,25 @@ public class TravelPoint : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
+        triggered = true; // Set the flag to true when an object enters the trigger
+
         // Check if the other object is a car agent
         CarAgent carAgent = other.GetComponent<CarAgent>();
-        if (carAgent != null)
+        if (carAgent != null && carAgent.destination.pointName == pointName)
         {
+
             // Set the car's destination to this travel point's successor
             TravelPoint successor = getSuccessor();
             if (successor != null)
             {
                 carAgent.destination = successor;
             }
-        }  
+        }
+    }
+    
+    private void OnTriggerExit(Collider other)
+    {
+        // Reset the flag when the object exits the trigger
+        triggered = false;
     }
 }
