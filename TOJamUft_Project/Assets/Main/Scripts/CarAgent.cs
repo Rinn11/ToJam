1650686b2eebb94484e03f4542e27c557e0521ac
M1ttttw <matt_tay2003@outlook.com>
2025-07-09 02:06:30 -0400

    Car Routing is Completely Done!

--- TOJamUft_Project/Assets/Main/Scripts/CarAgent.cs
+++ TOJamUft_Project/Assets/Main/Scripts/CarAgent.cs
@@ -4,6 +4,7 @@ public class CarAgent : MonoBehaviour
 {
     public TravelPoint destination; // The destination point the car should move towards
     public float speed; // Speed of the car
+    public float yOffset; // In case the direction needs to be adjusted.
 
     private void Update()
     {
