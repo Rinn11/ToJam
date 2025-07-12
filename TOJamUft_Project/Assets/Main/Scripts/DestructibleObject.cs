00700f6bd83d8755e21708eec8b7bd7a6aeabbe8
M1ttttw <matt_tay2003@outlook.com>
2025-07-11 23:11:19 -0400

    Implemented Cascading Destructible Objects
    This is where pushing one object against another object will lead to destruction. Previously the car specifically has to touch the object behind the object the player is pushing to trigger destruction.
    
    I also disabled some bounds.

--- TOJamUft_Project/Assets/Main/Scripts/DestructibleObject.cs
+++ TOJamUft_Project/Assets/Main/Scripts/DestructibleObject.cs
@@ -12,6 +12,8 @@ public class DestructibleObject : MonoBehaviour
 
     [Header("Respawn Settings")]
     public float respawnDelay; // The delay in seconds to spawn back the object/obstacle
+    [HideInInspector]
+    public bool beingDestroyed = false;
 
     Vector3 originalPosition;
     Quaternion originalRotation;
@@ -36,6 +38,7 @@ public class DestructibleObject : MonoBehaviour
     {
         rb.isKinematic = false;
         acceptCollisions = false;
+        beingDestroyed = true;
 
         Renderer[] rends = GetComponentsInChildren<Renderer>();
         Material[] oldMaterials = new Material[rends.Length];
@@ -71,12 +74,16 @@ public class DestructibleObject : MonoBehaviour
 
         // Re enable everything.
         acceptCollisions = true;
+        beingDestroyed = false;
         pseudoEnable();
     }
 
     void OnCollisionEnter(Collision collision)
     {
-        if (acceptCollisions && (collision.gameObject.CompareTag("CopCar") || collision.gameObject.CompareTag("Player")))
+        // Check if the collision contains another destructible object that is currently being destroyed.
+        DestructibleObject otherDestructible = collision.gameObject.GetComponent<DestructibleObject>();
+
+        if (acceptCollisions && (collision.gameObject.CompareTag("CopCar") || collision.gameObject.CompareTag("Player") || (otherDestructible != null && otherDestructible.beingDestroyed)))
         {
             StartCoroutine(destructionCoroutine());
         }
