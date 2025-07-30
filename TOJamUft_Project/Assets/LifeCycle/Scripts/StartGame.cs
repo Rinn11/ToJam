4b695697fc6ec387ef86acdfb77ecb91ecbcce46
M1ttttw <matt_tay2003@outlook.com>
2025-07-30 03:25:34 -0400

    TitleScreen is pretty much done

--- TOJamUft_Project/Assets/LifeCycle/Scripts/StartGame.cs
+++ TOJamUft_Project/Assets/LifeCycle/Scripts/StartGame.cs
@@ -6,7 +6,6 @@
 
 using UnityEngine;
 using UnityEngine.Events;
-using UnityEngine.InputSystem;
 using UnityEngine.UI;
 
 public class StartGame : MonoBehaviour
@@ -15,21 +14,46 @@ public class StartGame : MonoBehaviour
     public GameObject ingameUI;
     public AudioSource[] audioSources;
 
-    public Image player1Img, player2Img;
+    public RawImage player1Img, player2Img;
     private bool player1Active, player2Active;
+
+    [Header("Title Screen Camera Settings")]
+    public GameObject titleScreenCamera;
+    public float rotationSpeed;
+    private Vector3 rotationVector;
+
+    [Header("Display2 Settings")]
+    public GameObject titleScreenCamera2;
+    public RawImage player1Img2, player2Img2;
+    private bool isDualMonitor = false;
+
+    [Header("Events")]
     public UnityEvent StartGameEvent;
-        
+
+
     void Start()
     {
         // Time.timeScale = 0;
-        
+
         ingameUI.SetActive(false);
         titleScreen.SetActive(true);
-        
+
         audioSources = GetComponents<AudioSource>();
+
+        // Generate a random vector for camera rotations
+        rotationVector = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), Random.Range(-1f, 1f));
+        rotationVector.Normalize();
+
+        int displayCount = Display.displays.Length;
+
+        // Activate all displays
+        for (int i = 1; i < displayCount; i++)
+            Display.displays[i].Activate();
+        isDualMonitor = displayCount >= 2;
+
     }
 
-    
+
     void Update()
     {
         if (Time.timeScale == 0)
@@ -42,6 +66,18 @@ public class StartGame : MonoBehaviour
                 ingameUI.SetActive(true);
             }
         }
+
+        // Add perturbation to the rotation vector
+        rotationVector += new Vector3(Random.Range(-0.01f, 0.01f), Random.Range(-0.01f, 0.01f), Random.Range(-0.01f, 0.01f));
+        rotationVector.Normalize(); // Normalize to keep the direction consistent
+
+        // Rotate the title screen camera in the direction of the random vector
+        titleScreenCamera.transform.Rotate(rotationVector, rotationSpeed * Time.deltaTime);
+        if (isDualMonitor)
+        {
+            // Rotate the second camera in the same way
+            titleScreenCamera2.transform.Rotate(rotationVector, rotationSpeed * Time.deltaTime);
+        }
     }
 
     public void ReadyGame()
@@ -50,11 +86,13 @@ public class StartGame : MonoBehaviour
         {
             player1Active = true;
             player1Img.color = Color.green;
+            player1Img2.color = Color.green;
         }
         else if (!player2Active)
         {
             player2Active = true;
             player2Img.color = Color.green;
+            player2Img2.color = Color.green;
         }
 
         if (player1Active && player2Active)
