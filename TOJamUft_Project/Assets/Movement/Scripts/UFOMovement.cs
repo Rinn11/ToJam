7e8672badef1ffd16ec34d517945b9051118bb54
Rinn11 <davbrett@sheridancollege.ca>
2025-08-04 14:21:15 -0400

    More sound effects, MORE EFFECTS
    - added collision sounds
    - added UFO Sounds
    - added speed UI

--- TOJamUft_Project/Assets/Movement/Scripts/UFOMovement.cs
+++ TOJamUft_Project/Assets/Movement/Scripts/UFOMovement.cs
@@ -5,7 +5,9 @@
 // TODO: remove coupling with alcohol Manager
 
 using System;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.Events;
 using UnityEngine.InputSystem;
 using UnityEngine.UI;
@@ -26,6 +28,10 @@ public class UFOMovement : MonoBehaviour, IMovementModel
     public UnityEvent onBrake;
     public UnityEvent onIdle;
 
+    private bool accerated = true;
+
+    public GameObject speedParticles;
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
@@ -37,6 +43,10 @@ public class UFOMovement : MonoBehaviour, IMovementModel
         rb.angularDamping = angularDragForce;
 
         movementModifier = MovementManager.GetComponent<IMovementModifier>();
+
+        audioSource = gameObject.AddComponent<AudioSource>();
+        audioSource.loop = true;
+        PlaySound(idleClip, "Idle");
     }
 
     public void ProcessInputs(float x, float y)
@@ -74,6 +84,29 @@ public class UFOMovement : MonoBehaviour, IMovementModel
         {
             rb.AddForce(transform.forward * moveValue.y * useAccelerationForce);
         }
+
+
+        float deltaSpeed = rb.linearVelocity.magnitude - previousSpeed;
+
+        if (rb.linearVelocity.magnitude < speedThreshold)
+        {
+            PlaySound(idleClip, "Idle");
+            accerated = true;
+        }
+        else if (deltaSpeed > changeThreshold && accerated && moveValue.y >0)
+        {
+            PlaySound(accelerateClip, "Accelerate", false);
+            accerated = false;
+        }
+        else if (moveValue.y <= 0)
+        {
+            PlaySound(decelerateClip, "Decelerate", false);
+            accerated = true;
+        }
+        else if (!audioSource.isPlaying && moveValue.y > 0)
+        {
+            PlaySound(moveClip, "Move");
+        }
     }
     
         
@@ -83,5 +116,33 @@ public class UFOMovement : MonoBehaviour, IMovementModel
         if (rb == null) return;
         // shoould move at consistent velocity
         rb.AddForce(transform.forward * boost * accelerationForce, ForceMode.Acceleration);
+
+        speedParticles.SetActive(true);
+
+        if (boost >= 0) { speedParticles.SetActive(false); }
+    }
+
+    public AudioClip idleClip;
+    public AudioClip accelerateClip;
+    public AudioClip moveClip;
+    public AudioClip decelerateClip;
+
+    public float speedThreshold = 10f;
+    public float changeThreshold = 30f;
+
+    private AudioSource audioSource;
+    private float previousSpeed = 0f;
+    private string currentState = "";
+
+    void PlaySound(AudioClip clip, string stateName, bool loop = true)
+    {
+        if (currentState == stateName) return;
+
+        currentState = stateName;
+        audioSource.loop = loop;
+        audioSource.clip = clip;
+        audioSource.Play();
     }
 }
+
+
